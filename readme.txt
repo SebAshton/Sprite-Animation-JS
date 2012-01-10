f335ee10f8646c75f47e0417feff0a84abbb7d9f
Seb <seb@sebashton.com>
2012-01-10 14:44:37 +0000

    initial commit
    starting to build example and adding animation js

---
+++ readme.txt
@@ -0,0 +1,7 @@
+Lightweight sprite animation class.
+
+The Animation class takes 3 parameters:
+
+1: id of element with sprite contained.
+2: array of co-ordinates that make up the sprite.
+3: interval between steps.
---
+++ script/animation.js
@@ -0,0 +1,48 @@
+var Animation;
+Animation = (function() {
+  function Animation(id, array, intervalRate) {
+    var self;
+    this.id = id;
+    this.array = array;
+    this.intervalRate = intervalRate;
+    self = this;
+    this.elem = document.getElementById(id);
+    this.active = false;
+    this.arrSize = this.array.length - 1;
+    this.arrIt = 0;
+    this.timer = null;
+    this.animate = function() {
+      self.setBackground(self.array[self.arrIt][0], self.array[self.arrIt][1]);
+      return self.iterate();
+    };
+  }
+  Animation.prototype.start = function() {
+    if (this.timer || this.active) {
+      return false;
+    }
+    this.active = true;
+    this.timer = setInterval(this.animate, parseInt(this.intervalRate));
+    return true;
+  };
+  Animation.prototype.stop = function() {
+    clearInterval(this.timer);
+    this.timer = null;
+    return this.active = false;
+  };
+  Animation.prototype.setBackground = function(x, y) {
+    return this.elem.style.backgroundPosition = '' + x + ' ' + y + '';
+  };
+  Animation.prototype.iterate = function() {
+    var newIt;
+    if (this.arrIt === this.arrSize) {
+      return this.setIterate(parseInt(0));
+    } else {
+      newIt = parseInt(this.arrIt) + 1;
+      return this.setIterate(newIt);
+    }
+  };
+  Animation.prototype.setIterate = function(num) {
+    return this.arrIt = num;
+  };
+  return Animation;
+})();
