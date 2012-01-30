7e6cde60f4ee724efddf16111b491cec5031b933
Seb <seb@sebashton.com>
2012-01-30 17:44:27 +0000

    Util fix

--- array_gen.php
+++ array_gen.php
@@ -1,23 +1,30 @@
 <?php
 
-$imgWidth = '';
-$imgHeight = '';
+/* START edit */
 
-$spriteCount = '';
-$spriteWidth = '';
-$spriteHeight = '';
+$imgWidth = 159;
+$imgHeight = 149;
+
+$spriteCount = 148;
+$spriteWidth = 1908;
+$spriteHeight = 1937;
+
+/* END edit */
+
 
 $s = 0;
+$spriteItt = $spriteCount - 1;
 
 echo '[';
 
-for ($i = 0; $i < $imgHeight; $i = $i+$spriteHeight) {
-    for ($j = 0; $j < $imgWidth; $j = $i+$spriteWidth) {
-    	while ($s <= $spriteCount){
-    		echo '['.$i.','.$j.']';
-    		if ($s != $spriteCount){
-    			echo ',';
-    		}
+for ($i = 0; $i < $spriteHeight; $i=$i+$imgHeight) {
+    for ($j = 0; $j < $spriteWidth; $j=$j+$imgWidth) {
+    	if ($s < $spriteCount){
+     		echo '[-'.$j.',-'.$i.']';
+    		if ( $s < $spriteItt ){
+    			echo ',';    			
+     		} 
+			$s++;
     	}
 	}
 }
